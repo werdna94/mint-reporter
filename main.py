87876e6f53bd1e3dea5672112e980627b654fcbc
Andrew Gwilliam <andrewthemandrew32@gmail.com>
2023-04-08 17:29:08 -0600

    add mintapi successful login

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import mintapi
+import os
+
+
 def main():
-    print("Hello, world")
+    mint = mintapi.Mint(
+        os.environ.get('EMAIL'),
+        os.environ.get('PASSWORD'),
+        mfa_method='soft-token',
+        mfa_token=os.environ.get("MFA_TOKEN"),
+        headless=True,
+        wait_for_sync=True,
+        wait_for_sync_timeout=300,
+        fail_if_stale=True,
+    )
+
+    print(mint.get_account_data())
+
 
 if __name__ == '__main__':
     main()
